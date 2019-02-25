fd497bf3307d2e3c3ac1311ae1d5c586f465fb4c
seonk1211 <seonk94@naver.com>
2019-02-25 22:37:01 +0900

    unit list update

--- src/service/unit.js
+++ src/service/unit.js
@@ -44,6 +44,13 @@ const cost2 = [
         level: 1,
         img: 'https://s3.namuwikiusercontent.com/s/bbebc4f9826c5dddce8d71c5856665ba3e1897d57ca261d87f85992a53a25ab24436d0ef27f14b3e75fa7dd59ad51605ba79b6af3a6c3b7507fd574189868691212e7ec3cd1b892d0d56945fd3ead916db7f83bfc6de7e3cd30645bdd7b2bdf7'
     },
+    {
+        cost: '2$',
+        name: '',
+        color: 'black',
+        level: 1,
+        img: ''
+    },
 ]
 
 const cost3 = [
@@ -54,13 +61,69 @@ const cost3 = [
         level: 1,
         img: 'https://s3.namuwikiusercontent.com/s/4d57a880b9db5fb4d0e2c310b7900498f33b89c7aa322573ff8e58d7f7cee3b62adaf3bac78589b81c1e633d18822173f9242c11aa57b2fb090269968137ce6afae482c492d72ff61be48427443a4290c5078b1f69a76c9f63a08a4bed0376c3'
     },
-    /* {
+    {
         cost: '3$',
-        name: 'Abaddon',
+        name: 'Shadow Fiend',
         color: 'blue',
         level: 1,
-        img: ''
-    } */
+        img: 'https://w.namu.la/s/a6d53f635002e5b2eb5112da26d1a14bf1ad754c430754c542f2b55f73180df9d492ea1fc42a5156563d65f7a50a555fb7a728a94fea2c7767a075263cccb701c5298bef2f2cbba6fcb059423ca87de7d4be70b22f24f70663e49cfd9808a9ac'
+    },
+    {
+        cost: '3$',
+        name: 'Lycan',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/8d56a51466c10ad965ccbdec1db1495e89ddcdb57050bb4158c4a42061cfe318e6d93afc8004f40413fb3f9d39b61e49f0a94fa77daecc057f5e04817a2383159657aa9a0e51ccf0bd23a36af3f040e9d430ede1643c4509587921422e420dab'
+    },
+    {
+        cost: '3$',
+        name: 'Slark',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/2d01e872a23bf247bb354fe748032138cf72f6517f795d931e57d8ec78a51fe4e7d42373f60331001f2904bc0cd6fb1dd73994d14b58aedd3fc360758f016f63fe5bc1d5c06388da21fd83ba4178ba63e2ccb67325df9d64187d3334ced42dcb'
+    },
+    {
+        cost: '3$',
+        name: 'Phantom Assassin',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/59e1cc171814a17ed40bdd15f3ce663971b4bf1816487b57e70a73e437d89aac609cca6271c1b8f920e0b23e33e2676eb10ae7027aa9048571cb35a59e7629546b9f1f5c292f51568b98e2b5ce185639776b5c0fe907eed82fd7179cfa8a528f'
+    },
+    {
+        cost: '3$',
+        name: 'Terrorblade',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/fde998814375e5d0a8db607e33d1b323fdfb65b9435d6998e75a6ce2d44d37c203bb53cb0d346363fd73e320b95328d73778eafaff7fbc5162ae4f767af5569d9674fc3276e835db076c605feeca2f142c3cea115c07f1e7abe9de2b493ff709'
+    },
+    {
+        cost: '3$',
+        name: 'Sand King',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/c24b664e521e7c5d3bcd4b5baad61dabf2ec4447c4aaeb27cc5e9c772815f2282679aed0c969e00849c2e703699b23c452737cb40dadae88ba90c3ae08c7e7956dd7add6fc7780b3e1d2ffb66bfbeda1ee00cf1087654f406ad62794bc7bc896'
+    },
+    {
+        cost: '3$',
+        name: 'Lina',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/937499be1c0813d39d0ec906ddac53b5e8602f20915e145bad242e65c6a6343e99f8ca961081b8c619a2f8ba9df988d1bab22024d39edb5340b8c0dddeed8cff1a3c31ee0557c26b754958f79e3b56d87daeab4672e1fbe0b23dc982dfd7a3bf'
+    },
+    {
+        cost: '3$',
+        name: 'Viper',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/ecaecd2b2609b56f223e7ace928cad5023f528b73e348b3d9b8c077ce24cf72bc3defd28ab8fb05907dca9855770f0a364249dd48e4436159700ee6518fb914dfd5c7b497edf7d50c68ff48ade21e5d5d2a2d70dac8ce9e6a148b10fa4e11c73'
+    },
+    {
+        cost: '3$',
+        name: 'Sniper',
+        color: 'blue',
+        level: 1,
+        img: 'https://w.namu.la/s/67d07dec7e1bd05f2b06c64a4afc266d624bad1bb4d9543898111d06077c64b90f23eae9a68d2ad639dfcbc124d3f472bddc967e832d515cd12e6dc7af0e340872a17f7d89d7522a9c0c8ef03e09614a3a3aa25626428a96ca4691d34f17410e'
+    }
 ]
 
 const cost4 = [
@@ -69,8 +132,78 @@ const cost4 = [
         name: 'Dragon Knight',
         color: 'pink',
         level: 1,
-        img: 'https://s3.namuwikiusercontent.com/s/ef91802d4f840f65ecd080e3a80f56a927281d0d3a2e920a1718753009fc6fc07b368884996674905bc688460b28b78072f7070dcd24c353a975483e7531ef560f8381f70585afab914d124267115a073ed4bdbf0c6d4e84b56e0f1783aa987b'
-    }
+        img: 'https://w.namu.la/s/ef91802d4f840f65ecd080e3a80f56a927281d0d3a2e920a1718753009fc6fc07b368884996674905bc688460b28b78072f7070dcd24c353a975483e7531ef562f2bfb831e526553b1187f2d47e1397cae65d32fcc17ad12d487e970fc1e26b0'
+    },
+    {
+        cost: '4$',
+        name: 'Lone Druid',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/3f4b7d60306d457502bb280d1633c12d035eb75243fb7439fa37c2944a787b751f4f7e3dbc7670a499320b4dc19c506d4e1913af6ac8ddd29d248846f25fe4fad024c27dcfdff500e78da06595ef91dd7f9035effc9b4e9664015912d2ebe7ca'
+    },
+    {
+        cost: '4$',
+        name: 'Kunkka',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/a672899eb9580c4e654c887f3d9bfd2e4657b7ba8916f87b0ba5aa2a4f3bcc79b1613e6364adcdf9d91131b2ec5418a9096943f921d933a1f995b4f7bdaa1f5bae6465b37961328e6c329805a6d6bd552beaa8631db20a942ce1799aeb35809e'
+    },
+    {
+        cost: '4$',
+        name: 'Medusa',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/807190a07a3d05d0d0b6640245cf26fbb709a0793ddb0937d567b6752c5348e9145253f902ae480d61a50dd362bef276f66568f2f84afa5abc58a346155be631361906f4cfea16805b9891a99308362f2bb4ec0c34b5eb3e2899aaf771ce56a6'
+    },
+    {
+        cost: '4$',
+        name: 'Templar Assassin',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/a30dab9182926a29a9dbb57304491cafaaa6199f9ee10ba36268a2239e8a6c35951951383befaa78555707572accbb9ea75afe4817b3a92dd757f1aa411317440340d9a96a4fdc578176c111a9f0df482694f74236de64b000ce4143c41199a1'
+    },
+    {
+        cost: '4$',
+        name: 'Disruptor',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/93dcbecfbe96c41e4583470a7231285a9fc7a8d5b77e2729bbfc5325b562484be150643235376828f62107a7563ed7a47a596331a6630b6357c5940200be1c7e5aa43a3d278fbf4020820cce5b3ee0d02bb2eead78b9a96b781eaf3cd4330936'
+    },
+    {
+        cost: '4$',
+        name: 'Doom',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/7fdd80d8342f20265bbe4699a28a4b83c286b9714c923b17508bfd5cc4f230fa473f8036ef3e9e96141a8143bd3c9600cfedef05b4bb75b51c01995e996f2b455420ceff9a2cdb1ff9a910ca0ab06df94bfc60e987becbac03a0207726d99987'
+    },
+    {
+        cost: '4$',
+        name: 'Alchemist',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/c87f01a45356101874e225cce558e0251864edadb9782a4048edbb24627c2fef45e5f91805a88f8fb38d612f1375aed47c79f1445e8cc30d2c56c0c61efcefa32b89439aac26657cdd1de7eb8e2fc02138bb1caed1cdb400f2303739a262cc4d'
+    },
+    {
+        cost: '4$',
+        name: 'Necrophos',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/b4c266c37061de9e6fea255b5d72ed80203ea89c877afe3837582dbe58b72e238d1d597d330093336226f81ac4a24571e43fb00e101e9ba5495d6b67e39d8b588a1052647cf6778f6830d4d2368abfd64e80dcea613c59e66cbc1a900e42df39'
+    },
+    {
+        cost: '4$',
+        name: 'Troll Warlord',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/dafc1e3babb53dbb6c641642cabf138be8fd71dc663cfd996d0fd68d8d6ae50d8c3d0ca784aef970226b767920276f848c4ce59c57c0a086bfc44a037c9dd4fcbf8ef53b5bcd74450c39f9858c88b02c0d18877c1c7011b73a14730645152265'
+    },
+    {
+        cost: '4$',
+        name: 'Keeper of the Light',
+        color: 'pink',
+        level: 1,
+        img: 'https://w.namu.la/s/655a952f643826bb94c04ac53ea5c6d975067b27c17ac23df92f166dbbd6e96194d5cd3851b320d44224ea8d8cc0b51bfb2192ee06ba5073868b6e5500732c89da47beea442d3918eef02ddbf196be5ee0e976c8c62fb30bfe710eed1b6cfd34'
+    },
 ]
 
 const cost5 = [
@@ -79,7 +212,7 @@ const cost5 = [
         name: 'Lich',
         color: 'orange',
         level: 1,
-        img: 'https://s3.namuwikiusercontent.com/s/72ac6d8458de13338478d875fadefdb370fc036048ec50171ef7a0b5d171b6b2af0beba113bfc9e04a8e170d74ace78e8db482b681655126433c8e1390fedd8f1ac3b469f54bb93396c49f8fee09758514ac884f283a81beffcf68a052ce3c1d'
+        img: "require('../assets/unit/Lich.png')"
     },
     {
         cost: '5$',
