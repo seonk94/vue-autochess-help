920be5743e10198c6b2bd69dcb565c3ede98592a
Buba <34367843+seonk94@users.noreply.github.com>
2019-02-28 11:23:23 +0900

    Update SynergyTable.vue

--- src/components/SynergyTable.vue
+++ src/components/SynergyTable.vue
@@ -8,9 +8,10 @@
                         class="class-list"
                         v-for="item in specs"
                         :key="item.name"
+                        :style="{ background: item.effective}"
                         avatar
                     >
-                        <v-list-tile-content>
+                        <v-list-tile-content >
                             <v-tooltip top>
                                 <template #activator="effect">
                                     <v-list-tile-title v-on="effect.on" v-text="item.name"></v-list-tile-title>
@@ -39,6 +40,7 @@
                         class="class-list"
                         v-for="item in classes"
                         :key="item.name"
+                        :style="{ background: item.effective}"
                         avatar
                     >
 
@@ -64,6 +66,24 @@
 
                         
                     </v-list-tile>
+                    <v-list-tile class="class-list" avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title v-text="'색상설명'"></v-list-tile-title>
+                        </v-list-tile-content>
+                        효과1
+                        <v-list-tile-avatar class="class-list-avatar effect-one" :size="20">
+                            <span></span>
+                        </v-list-tile-avatar>
+                        효과2
+                        <v-list-tile-avatar class="class-list-avatar effect-middle" :size="20">
+                            <span></span>
+                        </v-list-tile-avatar>
+                        Full
+                        <v-list-tile-avatar class="class-list-avatar effect-finish"  :size="20">
+                            <span></span>
+                        </v-list-tile-avatar>
+                    </v-list-tile>
+
                 </v-list>
             </v-flex>
         </v-layout>
@@ -99,32 +119,32 @@
         },
         data:() => ({
             classes: [
-                { name: 'Assassin', img: [], index: 0, effect: '+10% (3), +20% (6) 확률로 암살자가 4배의 피해를 입습니다.'},
-                { name: 'Demon Hunter', img: [], index: 0, effect: '(1) 상대방이 악마 효과를 잃습니다.\n (2) 당신은 악마 효과를 절대 잃지 않습니다.'},
-                { name: 'Druid', img: [], index: 0, effect: '(2) 드루이드들은 2유닛들만으로 성이 될 수 있습니다.\n (4) 드루이드들은 2유닛들만으로 3성이 될 수 있습니다.'},
-                { name: 'Hunter', img: [], index: 0, effect: '모든 사냥꾼들에게 추가 공격력 +25%(3), +60%(6)'},
-                { name: 'Knight', img: [], index: 0, effect: '+25% (2), +35%(4), +45%(6) 확률로 기사들에게 방어막이 생성됩니다.'},
-                { name: 'Mage', img: [], index: 0, effect: '모든 적군에게 마법 방어력 -50(3), -30(6) 부여'},
-                { name: 'Mech', img: [], index: 0, effect: '모든 아군 기계에게 10 (2), 20(4) HP 리젠'},
-                { name: 'Shaman', img: [], index: 0, effect: '(2) 라운드가 시작 될 때 무작위 적군 1명에게 주술 시전'},
-                { name: 'Warlock', img: [], index: 0, effect: '모든 아군에게 흡혈 15% (3), 25% (6)'},
-                { name: 'Warrior', img: [], index: 0, effect: '모든 아군 전사에게 추가 방어력 +6 (3), +8 (6), +10 (9)'}
+                { name: 'Assassin', img: [], effective: '#fafafa', effectIndex: [3, 6], index: 0, effect: '+10% (3), +20% (6) 확률로 암살자가 4배의 피해를 입습니다.'},
+                { name: 'Demon Hunter', img: [], effective: '#fafafa', effectIndex: [1, 2], index: 0, effect: '(1) 상대방이 악마 효과를 잃습니다.\n (2) 당신은 악마 효과를 절대 잃지 않습니다.'},
+                { name: 'Druid', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '(2) 드루이드들은 2유닛들만으로 성이 될 수 있습니다.\n (4) 드루이드들은 2유닛들만으로 3성이 될 수 있습니다.'},
+                { name: 'Hunter', img: [], effective: '#fafafa', effectIndex: [3, 6], index: 0, effect: '모든 사냥꾼들에게 추가 공격력 +25%(3), +60%(6)'},
+                { name: 'Knight', img: [], effective: '#fafafa', effectIndex: [2, 4, 6], index: 0, effect: '+25% (2), +35%(4), +45%(6) 확률로 기사들에게 방어막이 생성됩니다.'},
+                { name: 'Mage', img: [], effective: '#fafafa', effectIndex: [3, 6], index: 0, effect: '모든 적군에게 마법 방어력 -50(3), -30(6) 부여'},
+                { name: 'Mech', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '모든 아군 기계에게 10 (2), 20(4) HP 리젠'},
+                { name: 'Shaman', img: [], effective: '#fafafa', effectIndex: [2], index: 0, effect: '(2) 라운드가 시작 될 때 무작위 적군 1명에게 주술 시전'},
+                { name: 'Warlock', img: [], effective: '#fafafa', effectIndex: [3, 6], index: 0, effect: '모든 아군에게 흡혈 15% (3), 25% (6)'},
+                { name: 'Warrior', img: [], effective: '#fafafa', effectIndex: [3, 6, 9], index: 0, effect: '모든 아군 전사에게 추가 방어력 +6 (3), +8 (6), +10 (9)'}
             ],
             specs: [
-                { name : 'Beast', img: [], index: 0, effect: '모든 아군에게 추가 공격력 +10% (2), +15% (4), +20% (6)'},
-                { name : 'Demon', img: [], index: 0, effect: '아군에 한 종류의 악마만 있을경우 50% 추가 순수 데미지'},
-                { name : 'Dwarf', img: [], index: 0, effect: '공격 범위 +300'},
-                { name : 'Ogre', img: [], index: 0, effect: '+10% Max Hp'},
-                { name : 'Dragon', img: [], index: 0, effect: '(3) 라운드가 시작 될 때 모든 아군 용이 마나 100을 가지고 시작'},
-                { name : 'Elementals', img: [], index: 0, effect: '모든 아군 원소 (2), 전체 (4) 를 공격하는 근접 적은 30% 확률로 돌이 됨'},
-                { name : 'Elf', img: [], index: 0, effect: '모든 아군 엘프에게 회피 +25% (3), +25% (6)'},
-                { name : 'Goblin', img: [], index: 0, effect: '무작위 아군 1명 (3), 전체 아군 고블린 (6) 에게 +15 방어력과 10HP 리젠'},
-                { name : 'Human', img: [], index: 0, effect: '모든 아군 인간은 곡격할 때 20% (2), 25% (4), 30% (6) 확률로 적을 잠시동안 무장 해제'},
-                { name : 'Naga', img: [], index: 0, effect: '모든 아군에게 마법 방어력 +20 (2), +40 (4)'},
-                { name : 'Orc', img: [], index: 0, effect: '모든 아군 오크에게 체력 +250 (2), +350 (4)'}, 
-                { name : 'Troll', img: [], index: 0, effect: '모든 아군 트롤 (2), 전체 (4) 에게 공격속도 35'},
-                { name : 'Undead', img: [], index: 0, effect: '모든 적군에게 방어력 -5 (2), -7 (4)'},
-                { name : 'Satyr', img: [], index: 0, effect: '필드 유닛 은신'}
+                { name : 'Beast', img: [], effective: '#fafafa', effectIndex: [2, 4, 6], index: 0, effect: '모든 아군에게 추가 공격력 +10% (2), +15% (4), +20% (6)'},
+                { name : 'Demon', img: [], effective: '#fafafa', effectIndex: [1], index: 0, effect: '아군에 한 종류의 악마만 있을경우 50% 추가 순수 데미지'},
+                { name : 'Dwarf', img: [], effective: '#fafafa', effectIndex: [2], index: 0, effect: '공격 범위 +300'},
+                { name : 'Ogre', img: [], effective: '#fafafa', effectIndex: [1], index: 0, effect: '+10% Max Hp'},
+                { name : 'Dragon', img: [], effective: '#fafafa', effectIndex: [3], index: 0, effect: '(3) 라운드가 시작 될 때 모든 아군 용이 마나 100을 가지고 시작'},
+                { name : 'Elementals', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '모든 아군 원소 (2), 전체 (4) 를 공격하는 근접 적은 30% 확률로 돌이 됨'},
+                { name : 'Elf', img: [], effective: '#fafafa', effectIndex: [3, 6], index: 0, effect: '모든 아군 엘프에게 회피 +25% (3), +25% (6)'},
+                { name : 'Goblin', img: [], effective: '#fafafa', effectIndex: [3, 6], index: 0, effect: '무작위 아군 1명 (3), 전체 아군 고블린 (6) 에게 +15 방어력과 10HP 리젠'},
+                { name : 'Human', img: [], effective: '#fafafa', effectIndex: [2, 4, 6], index: 0, effect: '모든 아군 인간은 곡격할 때 20% (2), 25% (4), 30% (6) 확률로 적을 잠시동안 무장 해제'},
+                { name : 'Naga', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '모든 아군에게 마법 방어력 +20 (2), +40 (4)'},
+                { name : 'Orc', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '모든 아군 오크에게 체력 +250 (2), +350 (4)'}, 
+                { name : 'Troll', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '모든 아군 트롤 (2), 전체 (4) 에게 공격속도 35'},
+                { name : 'Undead', img: [], effective: '#fafafa', effectIndex: [2, 4], index: 0, effect: '모든 적군에게 방어력 -5 (2), -7 (4)'},
+                { name : 'Satyr', img: [], effective: '#fafafa', effectIndex: [1], index: 0, effect: '필드 유닛 은신'}
             ]
         }),
         mounted() {
@@ -149,20 +169,76 @@
         methods: {
             moveUnit(unit, status) {
                 let cls = this.classes.find(ele => this.equalName(ele.name, unit.class))
+                cls.effective = '#fafafa'
                 let clsImg = cls.img.find(ele => ele.name === unit.name);
                 clsImg.status = status
-
+                
+                let usedIndex = 0;
                 let spec
                 let specImg
-                Array.isArray(unit.spec) 
-                    ?   unit.spec.forEach(tmp => {
-                            spec = this.specs.find(ele => this.equalName(ele.name, tmp));
-                            specImg = spec.img.find(ele => ele.name === unit.name);
-                            specImg.status = status
-                        })
-                    :   spec = this.specs.find(ele => this.equalName(ele.name, unit.spec));
+
+                if(Array.isArray(unit.spec)) {
+                    unit.spec.forEach(tmp => {
+                        usedIndex = 0;
+                        spec = this.specs.find(ele => this.equalName(ele.name, tmp));
+                        spec.effective = '#fafafa'
                         specImg = spec.img.find(ele => ele.name === unit.name);
                         specImg.status = status
+                        console.log(spec)
+                        spec.img.forEach(ele => {
+                            if(ele.status === 'USED') usedIndex++;
+                        })
+                        spec.index = usedIndex;
+                        if(spec.effectIndex.length === 3) {
+                            if(spec.index >= spec.effectIndex[0]) spec.effective = '#91a7ff';
+                            if(spec.index >= spec.effectIndex[1]) spec.effective = '#3b5bdb';
+                            if(spec.index >= spec.effectIndex[2]) spec.effective = '#fd7e14'
+                        } else if(spec.effectIndex.length === 2) {
+                            if(spec.index >= spec.effectIndex[0]) spec.effective = '#91a7ff';
+                            if(spec.index >= spec.effectIndex[1]) spec.effective = '#fd7e14';
+                        } else if(spec.effectIndex.length === 1) {
+                            if(spec.index >= spec.effectIndex[0]) spec.effective = '#fd7e14';
+                        }
+                    })
+                } else {
+                    usedIndex = 0;
+                    spec = this.specs.find(ele => this.equalName(ele.name, unit.spec));
+                    spec.effective = '#fafafa'
+                    specImg = spec.img.find(ele => ele.name === unit.name);
+                    specImg.status = status
+                    spec.img.forEach(ele => {
+                        if(ele.status === 'USED') usedIndex++;
+                    })
+                    spec.index = usedIndex;
+                    if(spec.effectIndex.length === 3) {
+                        if(spec.index >= spec.effectIndex[0]) spec.effective = '#91a7ff';
+                        if(spec.index >= spec.effectIndex[1]) spec.effective = '#3b5bdb';
+                        if(spec.index >= spec.effectIndex[2]) spec.effective = '#fd7e14'
+                    } else if(spec.effectIndex.length === 2) {
+                        if(spec.index >= spec.effectIndex[0]) spec.effective = '#91a7ff';
+                        if(spec.index >= spec.effectIndex[1]) spec.effective = '#fd7e14';
+                    } else if(spec.effectIndex.length === 1) {
+                        if(spec.index === spec.effectIndex[0]) spec.effective = '#fd7e14';
+                    }
+                }
+                
+                usedIndex = 0;
+                cls.img.forEach(ele => {
+                    if(ele.status === 'USED') usedIndex++;
+                })
+                cls.index = usedIndex;
+                
+                if(cls.effectIndex.length === 3) {
+                    if(cls.index >= cls.effectIndex[0]) cls.effective = '#91a7ff';
+                    if(cls.index >= cls.effectIndex[1]) cls.effective = '#3b5bdb';
+                    if(cls.index >= cls.effectIndex[2]) cls.effective = '#fd7e14'
+                } else if(cls.effectIndex.length === 2) {
+                    if(cls.index >= cls.effectIndex[0]) cls.effective = '#91a7ff';
+                    if(cls.index >= cls.effectIndex[1]) cls.effective = '#fd7e14';
+                } else if(cls.effectIndex.length === 1) {
+                    if(cls.index >= cls.effectIndex[0]) cls.effective = '#fd7e14';
+                }
+                
             },
             initNone(ele, status) {
                 ele.img.forEach(img => {
@@ -190,5 +266,17 @@
     }
     .unit-none {
         opacity: 0.15;
+    }
+    .effect-one {
+        background: #91a7ff;
+        border-radius: 50%;
+    }
+    .effect-middle {
+        background: #3b5bdb;
+        border-radius: 50%;
+    }
+    .effect-finish {
+        background: #fd7e14;
+        border-radius: 50%;
     }
 </style>
